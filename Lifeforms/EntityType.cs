c29a7a5a6367a457f3b54e54459b4bb286517b3a
Shane McLean <sdmclean3@hotmail.com>
2017-10-13 10:16:03 +0200

    Added all examples to the project.

---
+++ Lifeforms/EntityType.cs
@@ -0,0 +1,14 @@
+﻿namespace Lifeforms
+{
+    /// <summary>
+    /// Contains type definitions of different tuple types.
+    /// </summary>
+    public static class EntityType
+    {
+        public const int POSITION = 1;
+        public const int LIFEFORM_STATS = 2;
+        public const int FOOD = 3;
+        public const int SPAWN = 4;
+        public const int SIGNAL = 5;
+    }
+}
